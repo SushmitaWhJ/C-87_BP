6f3c29d5bf0d42db2c641ef857a31d9dfcd38791
SushmitaWhJ <52954822+SushmitaWhJ@users.noreply.github.com>
2023-06-07 17:09:13 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAtx-0lz0sgGw0T24BBCKdXSivrOzFDKWI",
+  authDomain: "story-telling-app-31feb.firebaseapp.com",
+  databaseURL: "https://story-telling-app-31feb-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-31feb",
+  storageBucket: "story-telling-app-31feb.appspot.com",
+  messagingSenderId: "945125669273",
+  appId: "1:945125669273:web:e1cb603bce55bb2e733b08"
+};
